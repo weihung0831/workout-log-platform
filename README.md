236a414f7c096071df08e99f9a4b93df4d93d057
weihung0831 <drum831@gmail.com>
2026-05-22 16:56:16 +0800

    Initialize workout log platform

---
+++ README.md
@@ -0,0 +1,10 @@
+# Next.js App
+
+This is a [Next.js](https://nextjs.org) project.
+
+## Getting Started
+
+```bash
+npm install
+npm run dev
+```
---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
+      </main>
+    </div>
+  );
+}
